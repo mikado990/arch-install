c5740f1da96153f7fab65caba976bbbfbb09cf90
mikado990 <137442705+mikado990@users.noreply.github.com>
2024-06-01 15:03:59 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ set +a
 ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
 source $CONFIGS_DIR/setup.conf
 ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
-( arch-chroot /mnt $HOME/ArchTitus/scripts/1-setup.sh )|& tee 1-setup.log
-( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchTitus/scripts/2-user.sh )|& tee 2-user.log
-( arch-chroot /mnt $HOME/ArchTitus/scripts/3-post-setup.sh )|& tee 3-post-setup.log
+( arch-chroot /mnt $HOME/arch-install/scripts/1-setup.sh )|& tee 1-setup.log
+( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/arch-install/scripts/2-user.sh )|& tee 2-user.log
+( arch-chroot /mnt $HOME/arch-install/scripts/3-post-setup.sh )|& tee 3-post-setup.log
 cp -v *.log /mnt/home/$USERNAME
 
