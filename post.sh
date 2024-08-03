5375371c0f8ff4b36326514ce9040b739269ae27
mikado990 <137442705+mikado990@users.noreply.github.com>
2024-08-03 06:31:33 +0000

    Update post.sh
    Added more flatpaks like flatseal and dolphin

--- post.sh
+++ post.sh
@@ -9,9 +9,10 @@ sudo ufw default allow outgoing
 sudo ufw enable
 
 ## Flatpak section
-flatpak install flathub org.onlyoffice.desktopeditors io.gitlab.librewolf-community com.spotify.Client io.github.shiftey.Desktop
+flatpak install flathub org.onlyoffice.desktopeditors io.gitlab.librewolf-community com.spotify.Client org.kde.dolphin com.github.tchx84.Flatseal
 
 sudo ln -s /var/lib/flatpak/exports/bin/org.onlyoffice.desktopeditors /usr/bin/onlyoffice
 sudo ln -s /var/lib/flatpak/exports/bin/io.gitlab.librewolf-community /usr/bin/librewolf
 sudo ln -s /var/lib/flatpak/exports/bin/com.spotify.Client /usr/bin/spotify
-sudo ln -s /var/lib/flatpak/exports/bin/io.github.shiftey.Desktop /usr/bin/github
+sudo ln -s /var/lib/flatpak/exports/bin/org.kde.dolphin /usr/bin/dolphin
+sudo ln -s /var/lib/flatpak/exports/bin/com.github.tchx84.Flatseal /usr/bin/flatseal
